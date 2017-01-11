c1ce47c4c9a4df026796c4960f021a77380d339a
Mehri Russo <mehrimova@gmail.com>
2017-01-11 09:48:51 -0700

    working on algorithmns

---
+++ algorithms.js
@@ -0,0 +1,16 @@
+// write a function for O(1)
+var pets = ['cat', 'dog', 'rat'];
+ function findPets(pets) {
+}
+console.log(pets[1]);
+
+// write a function for O(n)
+var pets = ['cat', 'dog', 'rat'];
+ function findPets(pets) {
+ for (var i = 0; i < pets.length; i++) {
+  pets[i] = pets[i]
+}
+}
+console.log(pets[1]);
+
+// write a function for O(n^2)
